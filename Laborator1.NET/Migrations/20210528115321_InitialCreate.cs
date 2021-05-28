9e0620d7f088be1bfad4cf5188c001f7f0dc934c
imci0198 <imci0198@scs.ubbcluj.ro>
2021-05-28 16:32:43 +0300

    -removed migration -created tabels in DB

--- Laborator1.NET/Migrations/20210528115321_InitialCreate.cs
+++ Laborator1.NET/Migrations/20210528115321_InitialCreate.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-using System;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace Laborator1.NET.Data.Migrations
+namespace Laborator1.NET.Migrations
 {
-    public partial class CreateIdentitySchema : Migration
+    public partial class InitialCreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
